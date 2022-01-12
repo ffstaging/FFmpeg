867b60c60dde63ea033a7b0d0ec5ea599b2e4c4e
softworkz <softworkz@hotmail.com>
2022-01-12 07:04:10 +0100

    avfilter/overlaygraphicsubs: Add overlaygraphicsubs and graphicsub2video filters
    - overlaygraphicsubs (VS -> V)
      Overlay graphic subtitles onto a video stream
    
    - graphicsub2video {S -> V)
      Converts graphic subtitles to video frames (with alpha)
      Gets auto-inserted for retaining compatibility with
      sub2video command lines
    
    Signed-off-by: softworkz <softworkz@hotmail.com>

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -358,6 +358,7 @@ extern const AVFilter ff_vf_oscilloscope;
 extern const AVFilter ff_vf_overlay;
 extern const AVFilter ff_vf_overlay_opencl;
 extern const AVFilter ff_vf_overlay_qsv;
+extern const AVFilter ff_vf_overlaygraphicsubs;
 extern const AVFilter ff_vf_overlay_vaapi;
 extern const AVFilter ff_vf_overlay_vulkan;
 extern const AVFilter ff_vf_overlay_cuda;
@@ -546,6 +547,7 @@ extern const AVFilter ff_avf_showvolume;
 extern const AVFilter ff_avf_showwaves;
 extern const AVFilter ff_avf_showwavespic;
 extern const AVFilter ff_vaf_spectrumsynth;
+extern const AVFilter ff_svf_graphicsub2video;
 
 /* multimedia sources */
 extern const AVFilter ff_avsrc_amovie;

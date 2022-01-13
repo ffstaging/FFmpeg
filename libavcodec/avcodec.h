808f5894884f0ef73365674ce7a697e5ac46421a
softworkz <softworkz@hotmail.com>
2022-01-13 05:47:40 +0100

    avcodec/subtitles: Migrate subtitle encoders to frame-based API
    and provide a compatibility shim for the legacy api
    
    Signed-off-by: softworkz <softworkz@hotmail.com>

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -2995,9 +2995,12 @@ void av_parser_close(AVCodecParserContext *s);
  * @{
  */
 
+ /**
+  * @deprecated Use @ref avcodec_encode_subtitle2() instead.
+  */
+attribute_deprecated
 int avcodec_encode_subtitle(AVCodecContext *avctx, uint8_t *buf, int buf_size,
                             const AVSubtitle *sub);
-
 
 /**
  * @}
